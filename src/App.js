a92489618d132e474303e76d8a6d2b5141604fa7
muzahid604 <604muzahid@gmail.com>
2022-03-22 16:40:43 +0600

    product add practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 function App() {
-  const players = ['sakib', 'tamim', 'nasir', 'sabbir']
+  const players = ['muzahid', 'sakib', 'tamim', 'nasir', 'sabbir']
   const profession = ['they', ' are', 'cricketer']
   return (
     <div className="App">
       {
         players.map(player => <Person name={player} profession={profession} ></Person>)
       }
+      <Product name="laptop" prices="3033"></Product>
+
+      <Product name="phone" prices="33"></Product>
+
     </div>
+
   );
 }
+
 function Person(props) {
 
   return (<div className='player'>
@@ -20,6 +26,14 @@ function Person(props) {
   </div>)
 }
 
+function Product(props) {
+  return (
+    <div className='player'>
+      <h1>Name:{props.name}</h1>
+      <h2>price:{props.prices} </h2>
+    </div>
+  )
+}
 
 
 export default App;
